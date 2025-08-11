8cf9f774902fee8b8bf0f0ca3beba56e908d4f2b
SadikBoraErteni <ertenibora@gmail.com>
2025-08-11 13:58:21 +0300

    Added CORS settings. Requests from the React application are allowed.

--- src/main/java/com/calisanyonetimi/calisan_yonetim_web/CalisanYonetimWebApplication.java
+++ src/main/java/com/calisanyonetimi/calisan_yonetim_web/CalisanYonetimWebApplication.java
@@ -2,12 +2,26 @@ package com.calisanyonetimi.calisan_yonetim_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CalisanYonetimWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalisanYonetimWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalisanYonetimWebApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5175")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
